962e4d812a6f22624da5d55fcf5faba3ef8234af
Muhamad Reza  Abdul Rohim <reza.wikrama3@gmail.com>
2021-03-17 17:50:56 +0700

    add some packages

--- app/User.php
+++ app/User.php
@@ -10,30 +10,13 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    protected $table = 'user';
+
     protected $fillable = [
-        'name', 'email', 'password',
+        'nip', 'name', 'password', 'plain_password', 'email', 'phone', 'address', 'gender', 'role', 'status', 'image'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+        'password'
     ];
 }
