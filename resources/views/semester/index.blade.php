2bd64651f7656318b03f92c90f89e4ca536a534a
alfazulkarnainaziz <alfazulkarnainaziz@gmail.com>
2021-03-24 11:11:52 +0700

    date smester

--- resources/views/semester/index.blade.php
+++ resources/views/semester/index.blade.php
@@ -36,6 +36,30 @@
 @push('js')
 <script>
 $(function() {
+    $.fn.dataTable.render.moment = function ( from, to, locale ) {
+        // Argument shifting
+        if ( arguments.length === 1 ) {
+            locale = 'en';
+            to = from;
+            from = 'YYYY-MM-DD';
+        }
+        else if ( arguments.length === 2 ) {
+            locale = 'en';
+        }
+     
+        return function ( d, type, row ) {
+            if (! d) {
+                return type === 'sort' || type === 'type' ? 0 : d;
+            }
+     
+            var m = window.moment( d, from, locale, true );
+     
+            // Order and type get a number value from Moment, everything else
+            // sees the rendered value
+            return m.format( type === 'sort' || type === 'type' ? 'x' : to );
+        };
+    };
+
     var table = $('#table').DataTable({
         processing: true,
         serverSide: true,
@@ -49,8 +73,8 @@ $(function() {
         ordering:false,
         columns: [
             { data: 'nama_semester', name: 'nama_semester' },
-            { data: 'from', name: 'from' },
-            { data: 'to', name: 'to' },
+            { name: 'from', data: 'from', render: $.fn.dataTable.render.moment( 'DD-MM-YYYY' ) },
+            { name: 'to', data: 'to', render: $.fn.dataTable.render.moment( 'DD-MM-YYYY' ) },
             { data: 'action', name: 'action' ,searchable:false}
         ]
     });
